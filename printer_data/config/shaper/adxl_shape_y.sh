f4ae43ca238d5a88ba45185a528346dc72cc4658
BryantW67 <45110554+BryantW67@users.noreply.github.com>
2024-05-30 13:54:41 -0400

    adxl_shape_y.sh modified - 30/05/24 - 13:54:39

--- printer_data/config/shaper/adxl_shape_y.sh
+++ printer_data/config/shaper/adxl_shape_y.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 ~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o ~/printer_data/config/shaper/shaper_calibrate_y.png
+python3 ~klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /tmp/shaper_calibrate_y.png
